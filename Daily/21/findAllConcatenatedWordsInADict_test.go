cc4469180e994cf4dbd338c5b0c90c803330e964
Synertry <jonny.somrak@gmail.com>
2022-10-06 23:59:25 +0200

    Daily #21

--- Daily/21/findAllConcatenatedWordsInADict_test.go
+++ Daily/21/findAllConcatenatedWordsInADict_test.go
@@ -1,9 +1,24 @@
 package Daily21
 
 import (
+	"math/rand"
 	"testing"
+	"time"
+	"unsafe"
 
 	"github.com/google/go-cmp/cmp"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var (
+	sliceOfString []string
+	src           = rand.NewSource(time.Now().UnixNano())
 )
 
 func TestFindAllConcatenatedWordsInADict(t *testing.T) {
@@ -29,4 +44,56 @@ func TestFindAllConcatenatedWordsInADict(t *testing.T) {
 	}
 }
 
-// TODO: Implement fuzz testing or random string slice generator
+func BenchmarkFindAllConcatenatedWordsInADict(b *testing.B) {
+	b.ReportAllocs()
+	benchmarks := []struct { // do not use maps! Order will be randomized
+		name string
+		len  int
+	}{
+		{"DictLen0", 0},
+		{"DictLen1", 1},
+		{"DictLen10", 10},
+		{"DictLen100", 100},
+		{"DictLen1000", 1000},
+		{"DictLen10000", 10000},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+			input, result := generateRandomStringSlice(bm.len), make([]string, bm.len)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				result = FindAllConcatenatedWordsInADict(input)
+			}
+			sliceOfString = result
+		})
+	}
+}
+
+func generateRandomStringSlice(length int) []string {
+	slice := make([]string, length)
+	for i := 0; i < length; i++ {
+		slice[i] = RandStringBytesMaskImprSrcUnsafe((i + 1) / 2)
+	}
+	return slice
+}
+
+// RandStringBytesMaskImprSrcUnsafe generates a random string of length n
+// Source: https://stackoverflow.com/a/31832326/5516320
+func RandStringBytesMaskImprSrcUnsafe(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return *(*string)(unsafe.Pointer(&b))
+}
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Synertry/RushDevsDaily/Daily/11"
+	"github.com/Synertry/RushDevsDaily/Daily/13"
+	"github.com/Synertry/RushDevsDaily/Daily/14"
+	"github.com/Synertry/RushDevsDaily/Daily/16"
+	"github.com/Synertry/RushDevsDaily/Daily/18"
+	"github.com/Synertry/RushDevsDaily/Daily/20"
+	"github.com/Synertry/RushDevsDaily/Daily/21"
+)
+
+var (
+	num            int
+	sliceOfInts    []int
+	sliceOfStrings []string
+	mat            [][]int
+)
+
+func main() {
+	// Daily 11
+	num = 23
+	fmt.Printf("Number of set bits in %d is:\n\t-> %d\n", num, Daily11.CountOneBits(num))
+	fmt.Printf("Number of set bits in %d is (without bitwise operation):\n\t-> %d\n\n", num, Daily11.CountOneBitsNonBitOps(num))
+
+	// Daily 13
+	sliceOfInts = []int{3, 5, 3, 3, 2, 4, 3}
+	fmt.Printf("Majority element of slice %v is:\n\t-> %d\n\n", sliceOfInts, Daily13.FindMajorElem(sliceOfInts))
+
+	// Daily 14
+	sliceOfInts = []int{2, 1, 2, 7, 3}
+	fmt.Printf("Maximum non-adjacent sum of slice %v is:\n\t-> %d\n\n", sliceOfInts, Daily14.MaxNonAdjacentSum(sliceOfInts))
+
+	// Daily 16
+	sliceOfInts = []int{1, 2, 3}
+	fmt.Printf("All generated subsets of slice %v are:\n\t-> %v\n\n", sliceOfInts, Daily16.GenerateAllSubsets(sliceOfInts))
+
+	// Daily 18
+	mat = [][]int{
+		{1, 3, 5, 8},
+		{10, 11, 15, 16},
+		{24, 27, 30, 31},
+	}
+	num = 120
+	fmt.Printf("In matrix %v the searched value exists:\n\t-> %d %v\n\n", mat, num, Daily18.SearchMatrix(mat, num))
+
+	// Daily 20
+	sliceOfInts = []int{1, 1, 3, 5, 7}
+	num = 1
+	fmt.Printf("In sorted list %v the searched number %d has indices:\n\t-> %v\n\n", sliceOfInts, num, Daily20.FindDupesInSortedList(sliceOfInts, num))
+
+	// Daily 21
+	sliceOfStrings = []string{"tech", "lead", "techlead", "cat", "cats", "dog", "catsdog"}
+	fmt.Printf("The concatenated words for %v:\n\t-> %v\n\n", sliceOfStrings, Daily21.FindAllConcatenatedWordsInADict(sliceOfStrings))
+}
